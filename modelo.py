42cf7e9ac6ca04dd5d952f3259cf07e78228d328
jmartins <jairo7martins@gmail.com>
2025-02-25 21:14:25 -0300

    comentarios

--- modelo.py
+++ modelo.py
@@ -2,17 +2,19 @@ import os
 import tarfile
 import urllib.request
 
+# Definindo o caminho do dataset
 DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml2/master/"
 HOUSING_PATH = os.path.join("datasets", "housing")
 HOUSING_URL = DOWNLOAD_ROOT + "datasets/housing/housing.tgz"
 
+# download do dataset
 def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
     os.makedirs(housing_path, exist_ok=True)
     tgz_path = os.path.join(housing_path, "housing.tgz")
-    urllib.request.urlretrieve(housing_url, tgz_path)
-    housing_tgz = tarfile.open(tgz_path)
-    housing_tgz.extractall(path=housing_path)
-    housing_tgz.close()
+    urllib.request.urlretrieve(housing_url, tgz_path) # Faz o download do arquivo
+    housing_tgz = tarfile.open(tgz_path) # Abre o arquivo
+    housing_tgz.extractall(path=housing_path) # Extrai o arquivo
+    housing_tgz.close()# Fecha o arquivo
 
 fetch_housing_data()
 
@@ -32,7 +34,7 @@ print(housing.describe()) # Mostra um resumo dos dados
 # Plotando os dados
 import matplotlib.pyplot as plt
 housing.hist(bins=50, figsize=(20,15))
-# plt.show()
+plt.show()
 
 
 # Dividindo os dados em treino e teste
@@ -48,7 +50,7 @@ housing["income_cat"] = pd.cut(housing["median_income"],
 
 housing["income_cat"].hist()
 
-# Dividindo os dados de acordo com a categoria de renda
+# Dividindo os dados de acordo com a categoria de renda de forma estratificada para garantir que as categorias sejam representativas no conjunto de treino e teste
 from sklearn.model_selection import StratifiedShuffleSplit
 
 split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
@@ -67,7 +69,7 @@ for set_ in (strat_train_set, strat_test_set):
 housing = strat_train_set.copy() # Copiando os dados de treino
 housing.plot(kind="scatter", x="longitude", y="latitude", alpha=0.1)  # alpha é a transparência dos pontos no gráfico
 plt.show()
-
+# Visualizando os dados com a população e o valor da casa
 housing.plot(kind="scatter", x="longitude", y="latitude", alpha=0.4,
             s=housing["population"]/100, label="population", figsize=(10,7),
             c="median_house_value", cmap=plt.get_cmap("jet"), colorbar=True) # s é o tamanho dos pontos no gráfico e c é a cor dos pontos 
@@ -76,15 +78,17 @@ plt.show()
 
 
 # Correlação entre os atributos
-# COnvertendo ocean_proximity para valores numéricos
+# Convertendo ocean_proximity para valores numéricos
 housing["ocean_proximity"] = pd.Categorical(housing["ocean_proximity"])
 housing["ocean_proximity"] = housing["ocean_proximity"].cat.codes
 
+# Calculando a correlação entre os atributos e o valor da casa
 corr_matrix = housing.corr()
 print(corr_matrix["median_house_value"].sort_values(ascending=False))
 
 from pandas.plotting import scatter_matrix
 
+# Atributos mais correlacionados com o valor da casa
 attributes = ["median_house_value", "median_income", "total_rooms", "housing_median_age"]
 scatter_matrix(housing[attributes], figsize=(12,8))
 plt.show()
